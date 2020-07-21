f4aebbd3c48302ab8788094ee882b39db4420bb9
kayhill <61764488+kayhill@users.noreply.github.com>
2020-07-21 12:30:40 -0400

    migration

--- commerce/settings.py
+++ commerce/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = '6ps8j!crjgrxt34cqbqn7x&b3y%(fny8k8nh21+qa)%ws3fh!q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
