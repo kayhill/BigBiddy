281ce8a010f5847fed5e50f6016657473451e5dc
kayhill <61764488+kayhill@users.noreply.github.com>
2020-07-21 15:05:21 -0400

    settings update

--- commerce/settings.py
+++ commerce/settings.py
@@ -33,7 +33,7 @@ if os.path.isfile(dotenv_file):
 SECRET_KEY = '6ps8j!crjgrxt34cqbqn7x&b3y%(fny8k8nh21+qa)%ws3fh!q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['0.0.0.0', 'localhost', 'bigbiddy.herokuapp.com', '127.0.0.1']
 
